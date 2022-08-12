da170672b2479ec8fa1870973d0f1c481ca998e7
Tiangang Cui <tiangang.cui@monash.edu>
2022-08-12 15:13:54 +1000

    update

--- tomo2d/example_pet.m
+++ tomo2d/example_pet.m
@@ -15,13 +15,13 @@ if isfile('setup.mat') && ~renew_setup
     load('setup.mat');
 else
 
-    tomo_opt = tomo_options('mesh_size', 64, 'angle', pi*2, 'n_sourc', 100, 'Is',  50);
+    tomo_opt = tomo_options('mesh_size', 32, 'angle', pi*2, 'n_sourc', 50, 'n_detec', 100, 'd_width', 1, 'Is',  100);
 
-    %inv_opts = inverse_options('cov_type', 'MRF', 'mean', 0, 'k', 100, 'cond', [1, 1, 0], 'sigma', 4, ...
-    %    'test_type', 'CF', 'test_base', 2, 'test_range', -4);
+    %inv_opts = inverse_options('cov_type', 'MRF', 'mean', 0, 'k', 5, 'cond', [1, 1, 0], 'sigma', 2, ...
+    %    'test_type', 'CF', 'test_base', 2, 'test_range', -7);
 
-    inv_opts = inverse_options('cov_type', 'GP', 'mean', 0, 'scale', 200, 'power', 1, 'sigma', 2, ...
-        'test_type', 'CF', 'test_base', 2, 'test_range', -5);
+    inv_opts = inverse_options('cov_type', 'GP', 'mean', 0, 'scale', 50, 'power', 1, 'sigma', 1, ...
+        'test_type', 'CF', 'test_base', 2, 'test_range', -7);
 
     [model, obs, prior] = setup_tomo(tomo_opt, inv_opts, '');
 
@@ -38,60 +38,4 @@ else
     surf(reshape(umap, tomo_opt.mesh_size, tomo_opt.mesh_size), 'edgecolor', 'none')
     
 end
-
-return
-
-mcmc_opt = mcmc_options('nstep', 1E2, 'sbatch', 1, 'lis_flag', true, 'sigma', -1);
-hess = @(v) eigen_PPFisher(model, obs, prior, v, 1E-2, obs.n_data);
-out_hmala = hmala(mlpost, hess, vmap, mcmc_opt);
-Hg = out_hmala.cross_g/out_hmala.nsample;
-
-%{
-ind = 1:1E2:out_hmala.nsample;
-
-Hvar = zeros(prior.dof);
-Hmean = zeros(prior.dof);
-for i = 1:numel(ind)
-    v = out_hmala.samples(:,i);
-    u = matvec_prior_L(prior, v) + prior.mean_u;
-    sol = forward_solve(model, u);
-    Hv = hessian(model, prior, sol, sol.d-obs.data);
-    Hmean = Hmean + Hv;
-    Hvar = Hvar + Hv*Hv;
-end
-
-Hmean = Hmean/numel(ind);
-Hvar = Hvar/numel(ind) - Hmean.^2;
-
-subplot(2,2,1)
-surf(Hg)
-
-subplot(2,2,2)
-surf(Hmean)
-
-subplot(2,2,3)
-surf(Hvar)
-%}
-
-%{
-v = vmap;
-u = matvec_prior_L(prior, v) + prior.mean_u;
-sol = forward_solve(model, u);
-
-tol = 1E-3;
-gp = zeros(prior.dof);
-gm = zeros(prior.dof);
-for i = 1:numel(v)
-    vp = v;
-    vp(i) = vp(i) + tol;
-    vm = v;
-    vm(i) = vm(i) - tol;
-    [~,~,~,gp(:,i)] = mlpost(vp);
-    [~,~,~,gm(:,i)] = mlpost(vm);
-end
-Hd = (gp - gm)/(2*tol);
-
-Hv = hessian(model, prior, sol, sol.d-obs.data);
-%}
-
 
