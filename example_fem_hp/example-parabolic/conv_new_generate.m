3c341fb453542b75941a127cc40c96f62b415c95
Tiangang Cui <tcui001@gmail.com>
2025-08-19 17:48:28 +1000

    generator new

--- example_fem_hp/example-parabolic/conv_new_generate.m
+++ example_fem_hp/example-parabolic/conv_new_generate.m
@@ -13,8 +13,8 @@ for i = 1:n_samples
     u = prior_random(prior, 1);
     sol = forward_solve(model, obs.true_u);
 
-    us = [us; u(:)];
-    qois = [qois; sol.q];
-    obss = [obss; sol.d];
+    us = [us, u(:)];
+    qois = [qois, sol.q];
+    obss = [obss, sol.d];
 end
 toc
